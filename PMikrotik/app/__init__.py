c850fb9b8ce7549797162ae31217892dea65613b
vojtovic <78141571+vojtovic@users.noreply.github.com>
2023-12-01 12:07:55 +0100

    Update __init__.py

--- PMikrotik/app/__init__.py
+++ PMikrotik/app/__init__.py
@@ -3,7 +3,7 @@ import logging
 from flask import Flask
 from flask_appbuilder import AppBuilder, SQLA
 from flask_migrate import Migrate
-
+from flask_restful import Api
 """
  Logging configuration
 """
@@ -11,12 +11,12 @@ from flask_migrate import Migrate
 logging.basicConfig(format="%(asctime)s:%(levelname)s:%(name)s:%(message)s")
 logging.getLogger().setLevel(logging.DEBUG)
 
+
 app = Flask(__name__)
 app.config.from_object("config")
 db = SQLA(app)
 migrate = Migrate(app, db)
 appbuilder = AppBuilder(app, db.session)
-
 
 """
 from sqlalchemy.engine import Engine
